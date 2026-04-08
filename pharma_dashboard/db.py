77abe059c56f1b7ac19a80eaf7309399bba56236
Mark Haynie <mmlcj5@gmail.com>
2026-04-07 21:16:14 -0500

    Update database connection string with sslmode=require

--- pharma_dashboard/db.py
+++ pharma_dashboard/db.py
@@ -12,7 +12,11 @@ SUPABASE_KEY = os.getenv("SUPABASE_KEY") or st.secrets.get("SUPABASE_KEY")
 if not SUPABASE_URL or not SUPABASE_KEY:
     raise ValueError("Missing Supabase credentials.")
 
-DATABASE_URL = f"postgresql+psycopg2://postgres:{SUPABASE_KEY}@db.gdkzizmehfvfokwdeapi.supabase.co:5432/postgres"
+DATABASE_URL = (
+    f"postgresql+psycopg2://postgres:{SUPABASE_KEY}"
+    "@db.gdkzizmehfvfokwdeapi.supabase.co:5432/postgres"
+    "?sslmode=require"
+)
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
